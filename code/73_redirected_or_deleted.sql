4ceec54541caed429dc296f02c97ab200aa65b70
ImreSamu <ImreSamu@users.noreply.github.com>
2017-11-10 17:28:22 +0100

    start working on redirected_or_deleted wikidaid-s

---
+++ code/73_redirected_or_deleted.sql
@@ -0,0 +1,11 @@
+select 
+    id
+   ,properties->>'wof:name'                    as wof_name 
+   ,properties->'wof:concordances'->>'wd:id'   as wd_id
+from wof
+where
+  properties->'wof:concordances'->>'wd:id'  IS NOT NULL
+and
+  properties->'wof:concordances'->>'wd:id'   NOT IN
+ (select  data->>'id'::text as wd_id  FROM wikidata.wd )
+;

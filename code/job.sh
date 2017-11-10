a332ffc75f43d58c54a719055a0ab293db55e92c
ImreSamu <ImreSamu@users.noreply.github.com>
2017-11-10 17:08:11 +0100

    redirect output to "/var/www/html/wof-wiki-dw/output" for remote web browsing

--- code/job.sh
+++ code/job.sh
@@ -6,8 +6,15 @@ cd /wof
 STARTTIME=$(date +%s)
 STARTDATE=$(date +"%Y%m%dT%H%M")
 
-log_file=/wof/log/job_${STARTDATE}.log
+outputdir=/wof/output/${STARTDATE}
+export  outputdir=${outputdir}
+
+
+mkdir -p ${outputdir}
+log_file=${outputdir}/job_${STARTDATE}.log
 rm -f $log_file
+
+
 
 #  backup log from here ...
 exec &> >(tee -a "$log_file")
@@ -25,37 +32,38 @@ wget https://raw.githubusercontent.com/CartoDB/cartodb-postgresql/master/scripts
 psql -f CDB_TransformToWebmercator.sql
 
 
-mkdir -p /wof/log
+#mkdir -p /wof/l
 
-rm -rf /wof/log/joblog01
-rm -rf /wof/log/joblog02
-rm -rf /wof/log/joblog03
 
-time parallel --results /wof/log/joblog01 -k  < /wof/code/parallel_joblist_01_load_tables.sh
-time parallel --results /wof/log/joblog02 -k  < /wof/code/parallel_joblist_02_sql_processing.sh
-time parallel --results /wof/log/joblog03 -k  < /wof/code/parallel_joblist_03_reporting.sh
+rm -rf ${outputdir}/joblog01
+rm -rf ${outputdir}/joblog02
+rm -rf ${outputdir}/joblog03
+
+time parallel --results ${outputdir}/joblog01 -k  < /wof/code/parallel_joblist_01_load_tables.sh
+time parallel --results ${outputdir}/joblog02 -k  < /wof/code/parallel_joblist_02_sql_processing.sh
+time parallel --results ${outputdir}/joblog03 -k  < /wof/code/parallel_joblist_03_reporting.sh
 
 time psql -f /wof/code/91_summary.sql
 
-rm -f /wof/reports/wof_wikidata_status.xlsx
+rm -f ${outputdir}/wof_wikidata_status.xlsx
 
-pgclimb -o /wof/reports/wof_wikidata_status.xlsx \
+pgclimb -o ${outputdir}/wof_wikidata_status.xlsx \
     -c "SELECT * FROM wof_extended_meta_status_country_summary;" \
     xlsx --sheet "meta_status_country_summary"
 
-pgclimb -o /wof/reports/wof_wikidata_status.xlsx \
+pgclimb -o ${outputdir}/wof_wikidata_status.xlsx \
     -c "SELECT * FROM wof_extended_status_summary;" \
     xlsx --sheet "_status_summary"
 
-pgclimb -o /wof/reports/wof_wikidata_status.xlsx \
+pgclimb -o ${outputdir}/wof_wikidata_status.xlsx \
     -c "SELECT * FROM wof_disambiguation_report;" \
     xlsx --sheet "disambiquation"
 
-pgclimb -o /wof/reports/wof_wikidata_status.xlsx \
+pgclimb -o ${outputdir}/wof_wikidata_status.xlsx \
     -c "SELECT * FROM wof_disambiguation_sum_report;" \
     xlsx --sheet "disambiquation_sum"
 
-pgclimb -o /wof/reports/wof_wikidata_status.xlsx \
+pgclimb -o ${outputdir}/wof_wikidata_status.xlsx \
     -c "SELECT * FROM wof_extreme_distance_report;" \
     xlsx --sheet "extreme_distance"
 
@@ -63,7 +71,7 @@ pgclimb -o /wof/reports/wof_wikidata_status.xlsx \
     -c "SELECT * FROM wof_extreme_distance_sum_report;" \
     xlsx --sheet "extreme_distance_sum"
 
-ls /wof/reports/* -la
+ls ${outputdir}/* -la
 
 echo "----------------------------------------------------------"
 echo "### Directory sizes: "
--- code/parallel_joblist_03_reporting.sh
+++ code/parallel_joblist_03_reporting.sh
@@ -1,2 +1,2 @@
-psql -vreportdir="/wof/reports" -f /wof/code/71_disambiquation.sql
-psql -vreportdir="/wof/reports" -f /wof/code/72_extreme_distance.sql
+psql -vreportdir="${outputdir}" -f /wof/code/71_disambiquation.sql
+psql -vreportdir="${outputdir}" -f /wof/code/72_extreme_distance.sql

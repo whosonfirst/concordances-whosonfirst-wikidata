fe5aa5b0d955744d8446d5aba6a73211fda0447a
ImreSamu <ImreSamu@users.noreply.github.com>
2017-11-13 00:12:55 +0100

    improve&fix natural-earth import

--- code/22_import_natural_earth.sh
+++ code/22_import_natural_earth.sh
@@ -4,6 +4,7 @@ echo """
     -- create natural-earth schema :
 
     CREATE SCHEMA IF NOT EXISTS ne;
+    DROP TABLE IF EXISTS ne.ne_10m_populated_places CASCADE;
 
     --
 """ | psql
@@ -12,17 +13,20 @@ ogr2ogr \
     -f Postgresql \
     -lco DIM=2 \
     -lco GEOMETRY_NAME=geometry \
+    -lco SCHEMA=ne \
     -nlt GEOMETRY \
     -overwrite \
     -progress \
     -s_srs EPSG:4326 \
     -t_srs EPSG:4326 \
-    -lco SCHEMA=ne \
     PG:"dbname=$PGDATABASE user=$PGUSER host=$PGHOST password=$PGPASSWORD port=$PGPORT" \
     "/wof/natural-earth-vector/10m_cultural/ne_10m_populated_places.shp"
 
 
 echo """
+
+    ANALYSE ne.ne_10m_populated_places ;
+
     -- test .. ;
 
     \d+ ne.ne_10m_populated_places;

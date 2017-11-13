9590dc3f932077e6e37a5ae2580fdc8ba5c53f4d
ImreSamu <ImreSamu@users.noreply.github.com>
2017-11-13 15:55:33 +0100

    add wikidata.wd_labels  and some fixes

--- code/60_wd_sql_views.sql
+++ code/60_wd_sql_views.sql
@@ -1,23 +1,23 @@
 drop table if exists wikidata.wd_names_preferred;
 create table wikidata.wd_names_preferred as
 with wd_names_iso2 as
- ( 
- select 
-       data->>'id'::text as wd_id 
-    , data->'labels'->jsonb_object_keys(data->'labels')->>'language'::text   as wd_lang    
-    , data->'labels'->jsonb_object_keys(data->'labels')->>'value'            as wof_value 
-    FROM wikidata.wd 
+ (
+ select
+       data->>'id'::text as wd_id
+    , data->'labels'->jsonb_object_keys(data->'labels')->>'language'::text   as wd_lang
+    , data->'labels'->jsonb_object_keys(data->'labels')->>'value'            as wof_value
+    FROM wikidata.wd
  )
-select 
+select
        wd.wd_id
       ,wd.wd_lang
-      ,langcodes.alpha3_b as wof_lang      
-      , case when langcodes.alpha3_b isnull 
+      ,langcodes.alpha3_b as wof_lang
+      , case when langcodes.alpha3_b isnull
              then 'name:' || wd.wd_lang         ||'_x_preferred'
-      		 else 'name:' || langcodes.alpha3_b ||'_x_preferred'   
-      	end      		
+      		 else 'name:' || langcodes.alpha3_b ||'_x_preferred'
+      	end
         as wof_property
-      ,wd.wof_value    
+      ,wd.wof_value
 FROM wd_names_iso2                      as wd
      left join codes.iso_language_codes as langcodes  on wd.wd_lang=langcodes.alpha2
 order by wd_id
@@ -31,17 +31,17 @@ ANALYZE wikidata.wd_names_preferred;
 
 
 create or replace view wikidata.wd_disambiguation
-as 
+as
 with p31 as (
-    select 
-     data->>'id'::text                                                                          as wikidataid 
-   , (jsonb_array_elements( data->'claims'->'P31' )->'mainsnak'->'datavalue'->'value'->>'id')   as wof_P31_value   
+    select
+     data->>'id'::text                                                                          as wikidataid
+   , (jsonb_array_elements( data->'claims'->'P31' )->'mainsnak'->'datavalue'->'value'->>'id')   as wof_P31_value
     FROM wikidata.wd
 )
 select * from P31
 --TODO: find (sub-)*subclass of a disambiguation page.
 where wof_P31_value = 'Q4167410'  -- disambiguation page
-; 
+;
 
 
 
@@ -49,26 +49,26 @@ where wof_P31_value = 'Q4167410'  -- disambiguation page
 
 
 create or replace view wikidata.wd_point
-as 
-with 
+as
+with
 p625 as (
-    select 
-         data->>'id'::text                              as wd_id 
+    select
+         data->>'id'::text                              as wd_id
         ,jsonb_array_elements( data->'claims'->'P625' ) as p625_object
     FROM wikidata.wd
 ),
 p625_extract as (
-	select 
+	select
 	      wd_id
-	     ,p625_object->'mainsnak'->'datavalue'->'value'->>'latitude'  as wd_latitude     
+	     ,p625_object->'mainsnak'->'datavalue'->'value'->>'latitude'  as wd_latitude
 	     ,p625_object->'mainsnak'->'datavalue'->'value'->>'longitude' as wd_longitude
-	     ,p625_object->'mainsnak'->'datavalue'->>'type'               as wd_type  
-	     ,p625_object->>'rank'                                        as wd_rank  
+	     ,p625_object->'mainsnak'->'datavalue'->>'type'               as wd_type
+	     ,p625_object->>'rank'                                        as wd_rank
 	from P625
 )
 select  wd_id
        ,wd_rank
-       ,case 
+       ,case
             -- https://www.wikidata.org/wiki/Help:Ranking
 	        when wd_rank='preferred'  then 100
 	        when wd_rank='normal'     then 1
@@ -76,27 +76,27 @@ select  wd_id
 	        else 0
         end as point_rank
        ,ST_SetSRID(ST_MakePoint( wd_longitude::float8, wd_latitude::float8),4326) as wd_point
-from p625_extract    
+from p625_extract
 --limit 1000
-; 
+;
 
 
 
 drop table if exists wikidata.wd_rank_point;
 create table wikidata.wd_rank_point as
-SELECT 
+SELECT
      wd_id
     ,wd_rank
     ,wd_point
-    ,point_rank 
-FROM 
+    ,point_rank
+FROM
     (
         SELECT *
-         , ROW_NUMBER() OVER (PARTITION BY wd_id  order by point_rank DESC) AS Row_ID 
+         , ROW_NUMBER() OVER (PARTITION BY wd_id  order by point_rank DESC) AS Row_ID
         FROM wikidata.wd_point
     ) AS A
-WHERE Row_ID = 1 
-ORDER BY wd_id 
+WHERE Row_ID = 1
+ORDER BY wd_id
 ;
 
 
@@ -104,31 +104,40 @@ ORDER BY wd_id
 
 
 
-create or replace view wikidata.wd_sitelinks;
- select 
-      data->>'id'::text                                                     as wd_id 
-    , data->'sitelinks'->jsonb_object_keys(data->'sitelinks')->>'site'      as wd_site    
-    , data->'sitelinks'->jsonb_object_keys(data->'sitelinks')->>'title'     as wd_title 
-    , data->'sitelinks'->jsonb_object_keys(data->'sitelinks')->'badges'     as wd_badges   
-    , jsonb_array_length(data->'sitelinks'->jsonb_object_keys(data->'sitelinks')->'badges')        as wd_badges_num      
-    FROM wikidata.wd 
+create or replace view wikidata.wd_sitelinks
+ select
+      data->>'id'::text                                                     as wd_id
+    , data->'sitelinks'->jsonb_object_keys(data->'sitelinks')->>'site'      as wd_site
+    , data->'sitelinks'->jsonb_object_keys(data->'sitelinks')->>'title'     as wd_title
+    , data->'sitelinks'->jsonb_object_keys(data->'sitelinks')->'badges'     as wd_badges
+    , jsonb_array_length(data->'sitelinks'->jsonb_object_keys(data->'sitelinks')->'badges')   as wd_badges_number
+    FROM wikidata.wd
 ;
-    
 
-create or replace view wikidata.wd_descriptions;
- select 
-      data->>'id'::text                                                             as wd_id 
-    , data->'descriptions'->jsonb_object_keys(data->'descriptions')->>'language'    as wd_language    
-    , data->'descriptions'->jsonb_object_keys(data->'descriptions')->>'value'       as wd_descriptions
-    FROM wikidata.wd 
+
+create or replace view wikidata.wd_descriptions
+ select
+      data->>'id'::text                                                             as wd_id
+    , data->'descriptions'->jsonb_object_keys(data->'descriptions')->>'language'    as wd_language
+    , data->'descriptions'->jsonb_object_keys(data->'descriptions')->>'value'       as wd_description
+    FROM wikidata.wd
 ;
- 
+
 
 --  it can be multiple language values , so wd_id + wd_language is not unique !! ;
-create or replace view wikidata.wd_aliases;
- select 
-      data->>'id'::text                                                   as wd_id 
-    , data->'aliases'->jsonb_object_keys(data->'aliases')->>'language'    as wd_language    
-    , data->'aliases'->jsonb_object_keys(data->'aliases')->>'value'       as wd_aliases
-    FROM wikidata.wd 
+create or replace view wikidata.wd_aliases
+ select
+      data->>'id'::text                                                   as wd_id
+    , data->'aliases'->jsonb_object_keys(data->'aliases')->>'language'    as wd_language
+    , data->'aliases'->jsonb_object_keys(data->'aliases')->>'value'       as wd_alias
+    FROM wikidata.wd
+;
+
+
+create or replace view wikidata.wd_labels
+ select
+       data->>'id'::text as wd_id
+    , data->'labels'->jsonb_object_keys(data->'labels')->>'language'   as wd_language
+    , data->'labels'->jsonb_object_keys(data->'labels')->>'value'      as wd_label
+    FROM wikidata.wd
 ;

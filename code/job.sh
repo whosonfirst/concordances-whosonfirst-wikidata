2c6d739389010c5973e36e1e4af2ed6f6d478742
ImreSamu <ImreSamu@users.noreply.github.com>
2017-11-15 15:06:12 +0100

    add country xls parallelisation

--- code/job.sh
+++ code/job.sh
@@ -31,74 +31,16 @@ wget https://raw.githubusercontent.com/CartoDB/cartodb-postgresql/master/scripts
 psql -f CDB_TransformToWebmercator.sql
 
 
-#mkdir -p /wof/l
-
-
 rm -rf ${outputdir}/joblog01
 rm -rf ${outputdir}/joblog02
 rm -rf ${outputdir}/joblog03
+rm -rf ${outputdir}/joblog04
 
 time parallel  --results ${outputdir}/joblog01 -k  < /wof/code/parallel_joblist_01_load_tables.sh
 time parallel  --results ${outputdir}/joblog02 -k  < /wof/code/parallel_joblist_02_sql_processing.sh
 time parallel  --results ${outputdir}/joblog03 -k  < /wof/code/parallel_joblist_03_reporting.sh
-
 time psql -f /wof/code/91_summary.sql
-
-rm -f ${outputdir}/wof_wikidata_status.xlsx
-pgclimb -o ${outputdir}/wof_wikidata_status.xlsx \
-    -c "SELECT * FROM wof_extended_status_summary;" \
-    xlsx --sheet "_status_summary_"
-
-pgclimb -o ${outputdir}/wof_wikidata_status.xlsx \
-    -c "SELECT * FROM wof_extended_meta_status_country_summary;" \
-    xlsx --sheet "meta_status_country_summary"
-
-pgclimb -o ${outputdir}/wof_wikidata_status.xlsx \
-    -c "SELECT * FROM wof_disambiguation_report;" \
-    xlsx --sheet "disambiquation"
-
-pgclimb -o ${outputdir}/wof_wikidata_status.xlsx \
-    -c "SELECT * FROM wof_disambiguation_sum_report;" \
-    xlsx --sheet "disambiquation_sum"
-
-pgclimb -o ${outputdir}/wof_wikidata_status.xlsx \
-    -c "SELECT * FROM wof_extreme_distance_report;" \
-    xlsx --sheet "extreme_distance"
-
-pgclimb -o ${outputdir}/wof_wikidata_status.xlsx \
-    -c "SELECT * FROM wof_extreme_distance_sum_report;" \
-    xlsx --sheet "extreme_distance_sum"
-
-pgclimb -o ${outputdir}/wof_wikidata_status.xlsx \
-    -c "SELECT * FROM wof_wd_redirects_report;" \
-    xlsx --sheet "wd_redirects"
-
-pgclimb -o ${outputdir}/wof_wikidata_status.xlsx \
-    -c "SELECT * FROM wof_wd_redirects_sum_report;" \
-    xlsx --sheet "wd_redirects_sum"
-
-
-/wof/code/92_country_export_to_xls.sh ${outputdir} AR
-/wof/code/92_country_export_to_xls.sh ${outputdir} HU
-
-/wof/code/92_country_export_to_xls.sh ${outputdir} US
-/wof/code/92_country_export_to_xls.sh ${outputdir} MX
-/wof/code/92_country_export_to_xls.sh ${outputdir} CA
-
-/wof/code/92_country_export_to_xls.sh ${outputdir} RU
-/wof/code/92_country_export_to_xls.sh ${outputdir} CN 
-
-/wof/code/92_country_export_to_xls.sh ${outputdir} AU
-/wof/code/92_country_export_to_xls.sh ${outputdir} NZ
-
-/wof/code/92_country_export_to_xls.sh ${outputdir} CH
-/wof/code/92_country_export_to_xls.sh ${outputdir} DE 
-/wof/code/92_country_export_to_xls.sh ${outputdir} AT
-
-/wof/code/92_country_export_to_xls.sh ${outputdir} ES 
-/wof/code/92_country_export_to_xls.sh ${outputdir} PT
-/wof/code/92_country_export_to_xls.sh ${outputdir} FR 
-
+time parallel  --results ${outputdir}/joblog04 -k  < /wof/code/parallel_joblist_04_country_reporting.sh
 
 ls ${outputdir}/* -la
 
---
+++ code/parallel_joblist_04_country_reporting.sh
@@ -0,0 +1,16 @@
+/wof/code/99_summary_export_to_xls.sh ${outputdir}
+/wof/code/92_country_export_to_xls.sh ${outputdir} AR
+/wof/code/92_country_export_to_xls.sh ${outputdir} HU
+/wof/code/92_country_export_to_xls.sh ${outputdir} US
+/wof/code/92_country_export_to_xls.sh ${outputdir} MX
+/wof/code/92_country_export_to_xls.sh ${outputdir} CA
+/wof/code/92_country_export_to_xls.sh ${outputdir} RU
+/wof/code/92_country_export_to_xls.sh ${outputdir} CN 
+/wof/code/92_country_export_to_xls.sh ${outputdir} AU
+/wof/code/92_country_export_to_xls.sh ${outputdir} NZ
+/wof/code/92_country_export_to_xls.sh ${outputdir} CH
+/wof/code/92_country_export_to_xls.sh ${outputdir} DE 
+/wof/code/92_country_export_to_xls.sh ${outputdir} AT
+/wof/code/92_country_export_to_xls.sh ${outputdir} ES 
+/wof/code/92_country_export_to_xls.sh ${outputdir} PT
+/wof/code/92_country_export_to_xls.sh ${outputdir} FR 

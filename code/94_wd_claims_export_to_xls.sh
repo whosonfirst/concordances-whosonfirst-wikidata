7ab5de2e5dd57080ece3764f82e570e98290c156
ImreSamu <ImreSamu@users.noreply.github.com>
2017-11-19 03:26:41 +0100

    add more records to test xls

--- code/94_wd_claims_export_to_xls.sh
+++ code/94_wd_claims_export_to_xls.sh
@@ -2,5 +2,5 @@
 
 rm -f ${outputdir}/wd_claims.xlsx
 pgclimb -o ${outputdir}/wd_claims.xlsx \
-    -c "SELECT * FROM wikidata.wd_claims LIMIT 10000" \
-    xlsx --sheet "_top10000_wd_claims"
+    -c "SELECT * FROM wikidata.wd_claims LIMIT 40000" \
+    xlsx --sheet "_top40000_wd_claims"

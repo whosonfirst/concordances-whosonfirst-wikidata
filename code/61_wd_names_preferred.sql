93e185bf0dc3cf4b0eae452270723b9f33d33cdd
ImreSamu <ImreSamu@users.noreply.github.com>
2017-12-19 00:43:14 +0100

    Fix wd_names * clean `wof_value`  " Jandaíra, Rio Grande do Norte" -> "Jandaíra" * remove cebuano values ! * temporary/quick fix:  filter out long language code  ( `zh-hans`)

--- code/61_wd_names_preferred.sql
+++ code/61_wd_names_preferred.sql
@@ -7,7 +7,8 @@ with wd_names_iso2 as
  select
       wd_id
     , data->'labels'->jsonb_object_keys(data->'labels')->>'language'::text   as wd_lang
-    , data->'labels'->jsonb_object_keys(data->'labels')->>'value'            as wof_value
+    -- , data->'labels'->jsonb_object_keys(data->'labels')->>'value'            as wof_value
+    ,(regexp_split_to_array( data->'labels'->jsonb_object_keys(data->'labels')->>'value'  , '[,()]'))[1]   as wof_value
     FROM wd.wdx
  )
 select
@@ -16,15 +17,16 @@ select
       ,langcodes.alpha3_b as wof_lang
       , case when langcodes.alpha3_b isnull
              then 'name:' || wd.wd_lang         ||'_x_preferred'
-      		 else 'name:' || langcodes.alpha3_b ||'_x_preferred'
+      	  	 else 'name:' || langcodes.alpha3_b ||'_x_preferred'
       	end
         as wof_property
       ,wd.wof_value
 FROM wd_names_iso2                      as wd
      left join codes.iso_language_codes as langcodes  on wd.wd_lang=langcodes.alpha2
+where wd.wd_lang != 'ceb'    
+    and length(coalesce( langcodes.alpha3_b ,wd.wd_lang )) = 3    
 order by wd_id
 ;
 
 create index on wd.wd_names_preferred ( wd_id ) WITH (fillfactor = 100);
 --ANALYZE         wd.wd_names_preferred;
-

e1ef9fe3ead26e65d79652aeb554f800dd95b449
ImreSamu <ImreSamu@users.noreply.github.com>
2017-12-21 06:22:26 +0100

    add NE import

--- code/22_import_natural_earth.sh
+++ code/22_import_natural_earth.sh
@@ -1,37 +1,57 @@
 #!/bin/bash
 
-echo """
-    -- create natural-earth schema :
+set -e
+set -u
 
-    CREATE SCHEMA IF NOT EXISTS ne;
-    DROP TABLE IF EXISTS ne.ne_10m_populated_places CASCADE;
+/wof/code/cmd_load_natural_earth.sh 10m_cultural ne_10m_populated_places
+/wof/code/cmd_load_natural_earth.sh 10m_cultural ne_10m_admin_0_countries
+/wof/code/cmd_load_natural_earth.sh 10m_cultural ne_10m_admin_0_disputed_areas
+/wof/code/cmd_load_natural_earth.sh 10m_cultural ne_10m_admin_0_map_subunits
+/wof/code/cmd_load_natural_earth.sh 10m_cultural ne_10m_admin_1_states_provinces
+/wof/code/cmd_load_natural_earth.sh 10m_cultural ne_10m_ports
+/wof/code/cmd_load_natural_earth.sh 10m_cultural ne_10m_time_zones
+/wof/code/cmd_load_natural_earth.sh 10m_cultural ne_10m_airports
 
-    --
-""" | psql -e
+/wof/code/cmd_load_natural_earth.sh 10m_cultural ne_10m_parks_and_protected_lands_area
+/wof/code/cmd_load_natural_earth.sh 10m_cultural ne_10m_parks_and_protected_lands_line
+/wof/code/cmd_load_natural_earth.sh 10m_cultural ne_10m_parks_and_protected_lands_point
+/wof/code/cmd_load_natural_earth.sh 10m_cultural ne_10m_parks_and_protected_lands_scale_rank
 
-ogr2ogr \
-    -f Postgresql \
-    -lco DIM=2 \
-    -lco GEOMETRY_NAME=geometry \
-    -lco SCHEMA=ne \
-    -nlt GEOMETRY \
-    -overwrite \
-    -progress \
-    -s_srs EPSG:4326 \
-    -t_srs EPSG:4326 \
-    PG:"dbname=$PGDATABASE user=$PGUSER host=$PGHOST password=$PGPASSWORD port=$PGPORT" \
-    "/wof/natural-earth-vector/10m_cultural/ne_10m_populated_places.shp"
+/wof/code/cmd_load_natural_earth.sh 10m_cultural ne_10m_admin_1_seams
+/wof/code/cmd_load_natural_earth.sh 10m_cultural ne_10m_admin_1_states_provinces
+/wof/code/cmd_load_natural_earth.sh 10m_cultural ne_10m_admin_1_states_provinces_lakes
+/wof/code/cmd_load_natural_earth.sh 10m_cultural ne_10m_admin_1_states_provinces_lines
+/wof/code/cmd_load_natural_earth.sh 10m_cultural ne_10m_admin_1_states_provinces_scale_rank
+/wof/code/cmd_load_natural_earth.sh 10m_cultural ne_10m_admin_1_states_provinces_scale_rank_minor_islands
 
+/wof/code/cmd_load_natural_earth.sh  10m_physical  ne_10m_antarctic_ice_shelves_lines         
+/wof/code/cmd_load_natural_earth.sh  10m_physical  ne_10m_antarctic_ice_shelves_polys         
+/wof/code/cmd_load_natural_earth.sh  10m_physical  ne_10m_coastline                           
+/wof/code/cmd_load_natural_earth.sh  10m_physical  ne_10m_geographic_lines                    
+/wof/code/cmd_load_natural_earth.sh  10m_physical  ne_10m_geography_marine_polys              
+/wof/code/cmd_load_natural_earth.sh  10m_physical  ne_10m_geography_regions_elevation_points  
+/wof/code/cmd_load_natural_earth.sh  10m_physical  ne_10m_geography_regions_points            
+/wof/code/cmd_load_natural_earth.sh  10m_physical  ne_10m_geography_regions_polys             
+/wof/code/cmd_load_natural_earth.sh  10m_physical  ne_10m_glaciated_areas          
+/wof/code/cmd_load_natural_earth.sh  10m_physical  ne_10m_lakes                    
+/wof/code/cmd_load_natural_earth.sh  10m_physical  ne_10m_lakes_europe             
+/wof/code/cmd_load_natural_earth.sh  10m_physical  ne_10m_lakes_historic           
+/wof/code/cmd_load_natural_earth.sh  10m_physical  ne_10m_lakes_north_america      
+/wof/code/cmd_load_natural_earth.sh  10m_physical  ne_10m_lakes_pluvial            
+/wof/code/cmd_load_natural_earth.sh  10m_physical  ne_10m_land                     
+/wof/code/cmd_load_natural_earth.sh  10m_physical  ne_10m_land_ocean_label_points  
+/wof/code/cmd_load_natural_earth.sh  10m_physical  ne_10m_land_ocean_seams            
+/wof/code/cmd_load_natural_earth.sh  10m_physical  ne_10m_land_scale_rank             
+/wof/code/cmd_load_natural_earth.sh  10m_physical  ne_10m_minor_islands               
+/wof/code/cmd_load_natural_earth.sh  10m_physical  ne_10m_minor_islands_coastline     
+/wof/code/cmd_load_natural_earth.sh  10m_physical  ne_10m_minor_islands_label_points  
+/wof/code/cmd_load_natural_earth.sh  10m_physical  ne_10m_ocean
+/wof/code/cmd_load_natural_earth.sh  10m_physical  ne_10m_ocean_scale_rank
+/wof/code/cmd_load_natural_earth.sh  10m_physical  ne_10m_playas
+/wof/code/cmd_load_natural_earth.sh  10m_physical  ne_10m_reefs
+/wof/code/cmd_load_natural_earth.sh  10m_physical  ne_10m_rivers_europe
+/wof/code/cmd_load_natural_earth.sh  10m_physical  ne_10m_rivers_lake_centerlines
+/wof/code/cmd_load_natural_earth.sh  10m_physical  ne_10m_rivers_lake_centerlines_scale_rank
+/wof/code/cmd_load_natural_earth.sh  10m_physical  ne_10m_rivers_north_america
 
-echo """
-
-    ANALYSE ne.ne_10m_populated_places ;
-
-    -- test .. ;
-
-    \d+ ne.ne_10m_populated_places;
-
-    select ogc_fid,scalerank,iso_a2,name  from ne.ne_10m_populated_places limit 10;
-
-    --
-""" | psql -e
+echo "END:======== natural earth loading ==========="
---
+++ code/cmd_load_natural_earth.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+
+tablepath=$1
+tablename=$2
+
+
+echo """
+    -- create natural-earth schema :
+
+    CREATE SCHEMA IF NOT EXISTS ne;
+    DROP TABLE IF EXISTS ne.${tablename} CASCADE;
+
+    --
+""" | psql -e
+
+ogr2ogr \
+    -f Postgresql \
+    -lco DIM=2 \
+    -lco GEOMETRY_NAME=geometry \
+    -lco SCHEMA=ne \
+    -lco precision=NO \
+    -nlt GEOMETRY \
+    -overwrite \
+    -progress \
+    -s_srs EPSG:4326 \
+    -t_srs EPSG:4326 \
+    PG:"dbname=$PGDATABASE user=$PGUSER host=$PGHOST password=$PGPASSWORD port=$PGPORT" \
+    "/wof/natural-earth-vector/${tablepath}/${tablename}.shp"
+
+
+echo """
+
+    ANALYSE ne.${tablename} ;
+
+    -- test .. ;
+
+    \d+ ne.${tablename};
+
+    -- \x
+    -- select *  from ne.${tablename} limit 2;
+    
+    --
+""" | psql -e

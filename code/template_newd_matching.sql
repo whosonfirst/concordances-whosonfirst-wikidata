0ae937bbead9607cf1e2c249aef1d050436b1b27
ImreSamu <ImreSamu@users.noreply.github.com>
2018-01-10 17:36:33 +0100

    add name_XX labels to NE export

--- code/template_newd_matching.sql
+++ code/template_newd_matching.sql
@@ -3,11 +3,11 @@ drop table if exists                      :ne_wd_match  CASCADE;
 EXPLAIN ANALYZE CREATE UNLOGGED TABLE     :ne_wd_match  as
     select
          ST_Distance( wd.wd_point_merc, ne.ne_geom_merc)::bigint  as _distance
-        ,wd.*  
-        ,ne.* 
+        ,wd.*
+        ,ne.*
         ,xxjarowinkler(ne.ne_name_has_num,wd.wd_name_has_num, ne.ne_una_name, wd.una_wd_name_en_clean)  as _xxjarowinkler
         ,  jarowinkler(ne.ne_una_name, wd.una_wd_name_en_clean)  as _jarowinkler
-      
+
         ,case  when ne.ne_name      = wd.wd_name_en_clean      then 'N1Full-name-match'
                when ne.ne_una_name  = wd.una_wd_name_en_clean  then 'N3Unaccent-name-match'
                when ne_name_array && wd_name_array             then 'N2Label-name-match'
@@ -15,15 +15,15 @@ EXPLAIN ANALYZE CREATE UNLOGGED TABLE     :ne_wd_match  as
                when jarowinkler(ne.ne_una_name, wd.una_wd_name_en_clean)>.971   then 'N5JaroWinkler-match'
          --    when (wd_concordances_array && ne_concordances_array) then 'N6only-Concordances-match'
                                                                else 'Nerr??-checkme-'
-         end as  _name_match_type    
- 
-    from :wd_input_table  as wd 
+         end as  _name_match_type
+
+    from :wd_input_table  as wd
         ,:ne_input_table  as ne
     where ( :mcond1
             :mcond2
             :mcond3
-          )  
-        
+          )
+
     order by ne.ogc_fid, _distance
 ;
 -- ANALYSE     :ne_wd_match  ;
@@ -33,52 +33,74 @@ EXPLAIN ANALYZE CREATE UNLOGGED TABLE     :ne_wd_match  as
 
 drop table if exists  :ne_wd_match_agg CASCADE;
 CREATE UNLOGGED TABLE :ne_wd_match_agg  as
-with wd_agg as 
+with wd_agg as
 (
     select ogc_fid,featurecla,ne_name, ne_wd_id
         ,  array_agg( wd_id     order by _distance) as a_wd_id
-        ,  array_agg(_distance  order by _distance) as a_wd_id_distance 
-        ,  array_agg(_name_match_type  order by _name_match_type ) as a_wd_name_match_type             
-    from :ne_wd_match 
+        ,  array_agg(_distance  order by _distance) as a_wd_id_distance
+        ,  array_agg(_name_match_type  order by _name_match_type ) as a_wd_name_match_type
+    from :ne_wd_match
     group by ogc_fid,featurecla, ne_name ,ne_wd_id
     order by ogc_fid,featurecla, ne_name ,ne_wd_id
 )
 , wd_agg_extended as
 (
  select wd_agg.*
-      ,case 
+      ,case
          when  array_length(a_wd_id,1) =1  then   a_wd_id[1]
            else NULL
         end as _suggested_wd_id
       ,array_length(a_wd_id,1)                      as wd_number_of_matches
-      ,distance_class(a_wd_id_distance[1]::bigint)  as _firstmatch_distance_category    
-     ,case 
-        when  array_length(a_wd_id,1) =1   and  array_length(a_wd_id_distance,1)=0  and ne_wd_id  = a_wd_id[1]                   then 'OK-VAL:validated,nodistance;'||a_wd_name_match_type[1]    
-        when  array_length(a_wd_id,1) =1   and  array_length(a_wd_id_distance,1)=0  and ne_wd_id != a_wd_id[1] and ne_wd_id !='' then 'OK-REP:suggested for replace,nodistance;'||a_wd_name_match_type[1] 
+      ,distance_class(a_wd_id_distance[1]::bigint)  as _firstmatch_distance_category
+     ,case
+        when  array_length(a_wd_id,1) =1   and  array_length(a_wd_id_distance,1)=0  and ne_wd_id  = a_wd_id[1]                   then 'OK-VAL:validated,nodistance;'||a_wd_name_match_type[1]
+        when  array_length(a_wd_id,1) =1   and  array_length(a_wd_id_distance,1)=0  and ne_wd_id != a_wd_id[1] and ne_wd_id !='' then 'OK-REP:suggested for replace,nodistance;'||a_wd_name_match_type[1]
         when  array_length(a_wd_id,1) =1   and  array_length(a_wd_id_distance,1)=0  and ne_wd_id != a_wd_id[1] and ne_wd_id  ='' then 'OK-ADD:suggested for add,nodistance;'||a_wd_name_match_type[1]
 
-        when  array_length(a_wd_id,1) =1   and  a_wd_id_distance[1] <= :safedistance and ne_wd_id  = a_wd_id[1]                   then 'OK-VAL:validated-'||a_wd_name_match_type[1]    
-        when  array_length(a_wd_id,1) =1   and  a_wd_id_distance[1] <= :safedistance and ne_wd_id != a_wd_id[1] and ne_wd_id !='' then 'OK-REP:suggested for replace-'||a_wd_name_match_type[1] 
+        when  array_length(a_wd_id,1) =1   and  a_wd_id_distance[1] <= :safedistance and ne_wd_id  = a_wd_id[1]                   then 'OK-VAL:validated-'||a_wd_name_match_type[1]
+        when  array_length(a_wd_id,1) =1   and  a_wd_id_distance[1] <= :safedistance and ne_wd_id != a_wd_id[1] and ne_wd_id !='' then 'OK-REP:suggested for replace-'||a_wd_name_match_type[1]
         when  array_length(a_wd_id,1) =1   and  a_wd_id_distance[1] <= :safedistance and ne_wd_id != a_wd_id[1] and ne_wd_id  ='' then 'OK-ADD:suggested for add-'||a_wd_name_match_type[1]
 
-        when  array_length(a_wd_id,1) =1   and  a_wd_id_distance[1] >  :safedistance then 'WARN:Extreme distance match (> :safedistance m)' 
-        when  array_length(a_wd_id,1) >1                                             then 'WARN:Multiple_match (please not import this!)' 
+        when  array_length(a_wd_id,1) =1   and  a_wd_id_distance[1] >  :safedistance then 'WARN:Extreme distance match (> :safedistance m)'
+        when  array_length(a_wd_id,1) >1                                             then 'WARN:Multiple_match (please not import this!)'
          else 'ER!R:check-me'
       end as _matching_category
   from wd_agg
 )
-select wd_agg_extended.* 
-    ,wd.a_wof_type
-    ,get_wdc_item_label(wd.data,'P31')                 as old_p31_instance_of
-    ,wdnew.p31_instance_of                             as new_p31_instance_of      
-    ,get_wdc_item_label(wd.data,'P17')                 as old_p17_country_id       
-    ,wdnew.p17_country_id                              as new_p17_country_id
+select wd_agg_extended.*
+    ,wdl.data->'labels'->'ar'->>'value' as  name_ar
+    ,wdl.data->'labels'->'bn'->>'value' as  name_bn
+    ,wdl.data->'labels'->'de'->>'value' as  name_de
+    ,wdl.data->'labels'->'en'->>'value' as  name_en
+    ,wdl.data->'labels'->'es'->>'value' as  name_es
+    ,wdl.data->'labels'->'fr'->>'value' as  name_fr
+    ,wdl.data->'labels'->'el'->>'value' as  name_el
+    ,wdl.data->'labels'->'hi'->>'value' as  name_hi
+    ,wdl.data->'labels'->'hu'->>'value' as  name_hu
+    ,wdl.data->'labels'->'id'->>'value' as  name_id
+    ,wdl.data->'labels'->'it'->>'value' as  name_it
+    ,wdl.data->'labels'->'ja'->>'value' as  name_ja
+    ,wdl.data->'labels'->'ko'->>'value' as  name_ko
+    ,wdl.data->'labels'->'nl'->>'value' as  name_nl
+    ,wdl.data->'labels'->'pl'->>'value' as  name_pl
+    ,wdl.data->'labels'->'pt'->>'value' as  name_pt
+    ,wdl.data->'labels'->'ru'->>'value' as  name_ru
+    ,wdl.data->'labels'->'sv'->>'value' as  name_sv
+    ,wdl.data->'labels'->'tr'->>'value' as  name_tr
+    ,wdl.data->'labels'->'vi'->>'value' as  name_vi
+    ,wdl.data->'labels'->'zh'->>'value' as  name_zh
+    ,wdl.a_wof_type
+    ,get_wdc_item_label (wd.data,'P31')                as old_p31_instance_of
+    ,get_wdc_item_label(wdl.data,'P31')                as new_p31_instance_of
+    ,get_wdc_item_label (wd.data,'P17')                as old_p17_country_id
+    ,get_wdc_item_label(wdl.data,'P17')                as new_p17_country_id
     ,get_wdlabeltext(wd_agg_extended.ne_wd_id)         as old_wd_label
     ,get_wdlabeltext(wd_agg_extended._suggested_wd_id) as new_wd_label
     ,is_cebuano(wd.data)                               as old_is_cebauno
 from wd_agg_extended
 left join wd.wdx             as wd     on wd_agg_extended.ne_wd_id=wd.wd_id
-left join :wd_input_table    as wdnew  on wd_agg_extended._suggested_wd_id=wdnew.wd_id   
+left join wd.wdx             as wdl    on wd_agg_extended._suggested_wd_id=wdl.wd_id
+-- left join :wd_input_table    as wdnew  on wd_agg_extended._suggested_wd_id=wdnew.wd_id
 ;
 ANALYSE :ne_wd_match_agg ;
 
@@ -86,47 +108,47 @@ ANALYSE :ne_wd_match_agg ;
 
 drop table if exists :ne_wd_match_notfound CASCADE;
 CREATE UNLOGGED TABLE         :ne_wd_match_notfound  as
-with 
+with
 extended_notfound as
-(   
+(
     select
          ne.ogc_fid
         ,ne.featurecla
-        ,ne.ne_name 
+        ,ne.ne_name
         ,ne.ne_wd_id
         ,ne.ne_una_name
         ,ne.ne_name_has_num
         ,ne.ne_name_array
         ,ne.ne_geom_merc
         ,get_wdlabeltext(ne.ne_wd_id)       as old_wd_label
-        ,ST_Distance(          
-            CDB_TransformToWebmercator( wd.geom)   
+        ,ST_Distance(
+            CDB_TransformToWebmercator( wd.geom)
             ,ne.ne_geom_merc)::bigint  as _old_distance
-        ,((ST_Distance(          
-            CDB_TransformToWebmercator( wd.geom)   
+        ,((ST_Distance(
+            CDB_TransformToWebmercator( wd.geom)
             ,ne.ne_geom_merc))/1000)::bigint  as _old_distance_km
         ,wd.a_wof_type
-        ,get_wdc_item_label(wd.data,'P31')    as old_p31_instance_of  
-        ,get_wdc_item_label(wd.data,'P17')    as old_p17_country_id   
-        ,is_cebuano(wd.data)                  as old_is_cebauno   
+        ,get_wdc_item_label(wd.data,'P31')    as old_p31_instance_of
+        ,get_wdc_item_label(wd.data,'P17')    as old_p17_country_id
+        ,is_cebuano(wd.data)                  as old_is_cebauno
     from :ne_input_table as ne
     left join wd.wdx as wd   on ne.ne_wd_id=wd.wd_id
-    where  ne.ogc_fid not in ( select ogc_fid from :ne_wd_match )  
+    where  ne.ogc_fid not in ( select ogc_fid from :ne_wd_match )
 )
 
-select 
-    case          
-            when _old_distance_km >=1500 then 'Notfound:DEL-Extreme distance 1500-    km'    
+select
+    case
+            when _old_distance_km >=1500 then 'Notfound:DEL-Extreme distance 1500-    km'
             when _old_distance_km >=700  then 'Notfound:DEL-Extreme distance  700-1500km'
             when _old_distance_km >=400  then 'Notfound:DEL-Extreme distance  400- 700km'
             when _old_distance_km >=200  then 'Notfound:DEL-Extreme distance  200- 400km'
-            when _old_distance_km >=50   then 'MAYBE:Notfound:Extreme distance 50- 200km'  
-            when _old_distance is null and  substr(ne_wd_id,1,1) = 'Q'       then 'MAYBE:Notfound:Current Wikidataid without coordinate'             
-            when _old_distance is not null                                   then 'MAYBE:Notfound-has wikidata, distance is near' 
+            when _old_distance_km >=50   then 'MAYBE:Notfound:Extreme distance 50- 200km'
+            when _old_distance is null and  substr(ne_wd_id,1,1) = 'Q'       then 'MAYBE:Notfound:Current Wikidataid without coordinate'
+            when _old_distance is not null                                   then 'MAYBE:Notfound-has wikidata, distance is near'
 
-            when ne_name is null then 'Notfound:ne_name is NULL'                            
+            when ne_name is null then 'Notfound:ne_name is NULL'
                                  else 'Notfound:has name - plase debug'
-    end as _matching_category  
+    end as _matching_category
     ,*
 from extended_notfound
 order by  ne_name
@@ -138,15 +160,15 @@ ANALYSE :ne_wd_match_notfound;
 
 drop table if exists  :ne_wd_match_agg_sum CASCADE;
 CREATE UNLOGGED TABLE  :ne_wd_match_agg_sum  as
-with 
+with
 _matched as (
-    select _matching_category,featurecla,  wd_number_of_matches, _firstmatch_distance_category, count(*) as N  
+    select _matching_category,featurecla,  wd_number_of_matches, _firstmatch_distance_category, count(*) as N
     from :ne_wd_match_agg
     group by  _matching_category,featurecla, wd_number_of_matches, _firstmatch_distance_category
-    order by  _matching_category,featurecla, wd_number_of_matches, _firstmatch_distance_category 
-), 
+    order by  _matching_category,featurecla, wd_number_of_matches, _firstmatch_distance_category
+),
 _notfound as (
-    select _matching_category,featurecla,  null::int as wd_number_of_matches, null::text as _firstmatch_distance_category, count(*) as N  
+    select _matching_category,featurecla,  null::int as wd_number_of_matches, null::text as _firstmatch_distance_category, count(*) as N
     from :ne_wd_match_notfound
     group by  _matching_category,featurecla,  wd_number_of_matches, _firstmatch_distance_category
     order by  _matching_category,featurecla,  wd_number_of_matches, _firstmatch_distance_category
@@ -162,25 +184,25 @@ ANALYSE :ne_wd_match_agg_sum ;
 \set _pct _pct
 drop table if exists  :ne_wd_match_agg_sum:_pct CASCADE;
 CREATE UNLOGGED TABLE          :ne_wd_match_agg_sum:_pct  as
-with 
- total AS (  select 
-            '-- total --' as _matching_category 
+with
+ total AS (  select
+            '-- total --' as _matching_category
            , sum(N) as N
-           , null::numeric(10,5) as pct 
-           from :ne_wd_match_agg_sum ) 
+           , null::numeric(10,5) as pct
+           from :ne_wd_match_agg_sum )
 ,msum  as (
     SELECT _matching_category
         , sum(N)       as N
     FROM :ne_wd_match_agg_sum
-    group by _matching_category 
-    order by _matching_category  
+    group by _matching_category
+    order by _matching_category
 )
-select msum._matching_category 
+select msum._matching_category
       ,msum.N
-      ,((100.0 * msum.N ) /total.N)::numeric(10,5) as pct  
+      ,((100.0 * msum.N ) /total.N)::numeric(10,5) as pct
 from msum, total
 union all
-  select    * 
+  select    *
   from total
 ;
 ANALYSE :ne_wd_match_agg_sum:_pct;
@@ -192,37 +214,37 @@ select * from :ne_wd_match_agg_sum:_pct;
 
 \set _suggestions _s
 drop table if exists    :ne_wd_match_notfound:_suggestions CASCADE;
-CREATE UNLOGGED TABLE   :ne_wd_match_notfound:_suggestions  as   
+CREATE UNLOGGED TABLE   :ne_wd_match_notfound:_suggestions  as
 with x as
 (
-select    
+select
          ne.ogc_fid   as _ogc_fid
         ,ST_Distance( wd.wd_point_merc, ne.ne_geom_merc)::bigint  as _distance
-        ,jarowinkler(ne.ne_una_name, wd.una_wd_name_en_clean)     as _jarowinkler         
-        ,wd.*   
+        ,jarowinkler(ne.ne_una_name, wd.una_wd_name_en_clean)     as _jarowinkler
+        ,wd.*
         ,ne.ogc_fid
         ,ne.featurecla
-        ,ne.ne_name 
+        ,ne.ne_name
         ,ne.ne_wd_id
         ,ne.ne_una_name
         ,ne.ne_name_has_num
         ,ne.ne_name_array
         ,ne.ne_geom_merc
         --,xxjarowinkler(ne.ne_name_has_num,wd.wd_name_has_num, ne.ne_una_name, wd.una_wd_name_en_clean)  as _xxjarowinkler
-    from :wd_input_table        as wd 
-        ,:ne_wd_match_notfound  as ne    
+    from :wd_input_table        as wd
+        ,:ne_wd_match_notfound  as ne
     where ( (ST_DWithin ( wd.wd_point_merc, ne.ne_geom_merc , :suggestiondistance ))
-          )    
+          )
     order by _ogc_fid, _distance, _jarowinkler desc
-)    
+)
 ,x_score as (
 select
-  case 
+  case
     when _distance=0 and (_jarowinkler is not null) then (nsitelinks/40) + 150 + (_jarowinkler*100)
     when _distance=0 and (_jarowinkler is null    ) then (nsitelinks/40) + 150 + 40
     when _distance>0 and (_jarowinkler is not null) then (nsitelinks/40) + 100 - (ln(_distance)*10) + (_jarowinkler*100)
-    when _distance>0 and (_jarowinkler is null    ) then (nsitelinks/40) + 100 - (ln(_distance)*10) + 40    
-  end  
+    when _distance>0 and (_jarowinkler is null    ) then (nsitelinks/40) + 100 - (ln(_distance)*10) + 40
+  end
   as _score
  ,*
  from x
@@ -230,11 +252,11 @@ select
 )
 ,x_top3 as (
 select  _ogc_fid,wd_id,_score,_distance,nsitelinks
-from     
-( select *, row_number() over (partition by _ogc_fid order by _score desc) as row_id  
-  from x_score 
-) as t 
-where   row_id <4 
+from
+( select *, row_number() over (partition by _ogc_fid order by _score desc) as row_id
+  from x_score
+) as t
+where   row_id <4
 order by _score desc, _ogc_fid
 )
 ,x_top3_agg as (
@@ -243,40 +265,40 @@ select  _ogc_fid as top3_ogc_fid
      ,array_agg(_score     order by _score desc)  as top3_score
      ,array_agg(_distance  order by _score desc)  as top3_distance
      ,array_agg(nsitelinks order by _score desc)  as top3_nsitelinks
-from x_top3    
+from x_top3
 group by top3_ogc_fid
 order by top3_ogc_fid
 )
 select t._ogc_fid as x_ogc_fid,t.wd_id as x_wd_id, t._score as x_score
-   ,case 
+   ,case
         when array_length(top3_score,1)=1 and t3a.top3_score[1]>140  then 'AOK:TOP_ONLY_ONE-'
         when array_length(top3_score,1)=1 and t3a.top3_score[1]>80   then 'MAYBE:TOP_ONLY_ONE'
         when array_length(top3_score,1)=1                            then 'XLOW:TOP_ONLY_ONE'
 
         when array_length(top3_score,1)>1 and ((t3a.top3_score[1]-t3a.top3_score[2]) / t3a.top3_score[1])>0.25 and t3a.top3_score[1]>140 then 'AOK:CLEARWIN'
         when array_length(top3_score,1)>1 and ((t3a.top3_score[1]-t3a.top3_score[2]) / t3a.top3_score[1])>0.25 and t3a.top3_score[1]>80  then 'MAYBE:CLEARWIN'
-        when array_length(top3_score,1)>1 and ((t3a.top3_score[1]-t3a.top3_score[2]) / t3a.top3_score[1])>0.25                           then 'XLOW:CLEARWIN'                
+        when array_length(top3_score,1)>1 and ((t3a.top3_score[1]-t3a.top3_score[2]) / t3a.top3_score[1])>0.25                           then 'XLOW:CLEARWIN'
 
-        when t3a.top3_score[1]>140 then 'MULTIWIN:AOK'    
-        when t3a.top3_score[1]>80  then 'MULTIWIN:MAYBE'             
+        when t3a.top3_score[1]>140 then 'MULTIWIN:AOK'
+        when t3a.top3_score[1]>80  then 'MULTIWIN:MAYBE'
                                    else 'MULTIWIN:XLOW'
     end as _rank
-   ,case 
+   ,case
      when ne_name is NULL  then 'no-ne-name'
                            else 'has-name  '
-    end as _namegrp                              
-   ,* 
-from 
-( select *, row_number() over (partition by _ogc_fid order by _score desc) as row_id  
-  from x_score 
+    end as _namegrp
+   ,*
+from
+( select *, row_number() over (partition by _ogc_fid order by _score desc) as row_id
+  from x_score
 ) as t
 left join x_top3_agg as t3a  on t._ogc_fid=t3a.top3_ogc_fid
-where row_id <2 
+where row_id <2
 order by t._ogc_fid
 ;
 
 
-select _namegrp,_rank, count(*) as N 
+select _namegrp,_rank, count(*) as N
 from :ne_wd_match_notfound:_suggestions
 group by _namegrp,_rank
 order by _namegrp,_rank

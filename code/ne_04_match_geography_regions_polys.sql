e99fc028d581e5bc14d113c3f5386df7e28ccf49
ImreSamu <ImreSamu@users.noreply.github.com>
2018-01-28 04:53:19 +0100

    extend (geography_regions_polys)   with notfound and pct table

--- code/ne_04_match_geography_regions_polys.sql
+++ code/ne_04_match_geography_regions_polys.sql
@@ -43,3 +43,87 @@ union all select * from newd.ne_wd_georegp_mountain_match_agg
 ;
 
 
+
+drop table if exists newd.ne_wd_match_geography_regions_polys_match_notfound CASCADE;
+create table         newd.ne_wd_match_geography_regions_polys_match_notfound as
+          select * from newd.ne_wd_georegp_continent_match_notfound
+union all select * from newd.ne_wd_georegp_delta_match_notfound
+union all select * from newd.ne_wd_georegp_island_match_notfound
+union all select * from newd.ne_wd_georegp_depression_match_notfound
+union all select * from newd.ne_wd_georegp_desert_match_notfound
+union all select * from newd.ne_wd_georegp_geoarea_match_notfound
+union all select * from newd.ne_wd_georegp_peninsula_match_notfound
+union all select * from newd.ne_wd_georegp_lakegrp_match_notfound
+union all select * from newd.ne_wd_georegp_valley_match_notfound
+union all select * from newd.ne_wd_georegp_plain_match_notfound
+union all select * from newd.ne_wd_georegp_wetland_match_notfound
+union all select * from newd.ne_wd_georegp_plateau_match_notfound
+union all select * from newd.ne_wd_georegp_isthmus_match_notfound
+union all select * from newd.ne_wd_georegp_tundra_match_notfound
+union all select * from newd.ne_wd_georegp_basin_match_notfound
+union all select * from newd.ne_wd_georegp_pencape_match_notfound
+union all select * from newd.ne_wd_georegp_coast_match_notfound
+union all select * from newd.ne_wd_georegp_mountain_match_notfound
+;
+ --
+
+
+
+
+
+\set ne_wd_match               newd.ne_wd_match_geography_regions_polys_match
+\set ne_wd_match_agg           newd.ne_wd_match_geography_regions_polys_match_agg
+\set ne_wd_match_agg_sum       newd.ne_wd_match_geography_regions_polys_match_agg_sum
+\set ne_wd_match_notfound      newd.ne_wd_match_geography_regions_polys_match_notfound
+
+
+drop table if exists  :ne_wd_match_agg_sum CASCADE;
+CREATE UNLOGGED TABLE  :ne_wd_match_agg_sum  as
+with
+_matched as (
+    select _matching_category,featurecla,  wd_number_of_matches, _firstmatch_distance_category, count(*) as N
+    from :ne_wd_match_agg
+    group by  _matching_category,featurecla, wd_number_of_matches, _firstmatch_distance_category
+    order by  _matching_category,featurecla, wd_number_of_matches, _firstmatch_distance_category
+),
+_notfound as (
+    select _matching_category,featurecla,  null::int as wd_number_of_matches, null::text as _firstmatch_distance_category, count(*) as N
+    from :ne_wd_match_notfound
+    group by  _matching_category,featurecla,  wd_number_of_matches, _firstmatch_distance_category
+    order by  _matching_category,featurecla,  wd_number_of_matches, _firstmatch_distance_category
+)
+
+           select * from _matched
+union all  select * from _notfound
+;
+
+ANALYSE :ne_wd_match_agg_sum ;
+
+
+\set _pct _pct
+drop table if exists  :ne_wd_match_agg_sum:_pct CASCADE;
+CREATE UNLOGGED TABLE          :ne_wd_match_agg_sum:_pct  as
+with
+ total AS (  select
+            '-- total --' as _matching_category
+           , sum(N) as N
+           , null::numeric(10,5) as pct
+           from :ne_wd_match_agg_sum )
+,msum  as (
+    SELECT _matching_category
+        , sum(N)       as N
+    FROM :ne_wd_match_agg_sum
+    group by _matching_category
+    order by _matching_category
+)
+select msum._matching_category
+      ,msum.N
+      ,((100.0 * msum.N ) /total.N)::numeric(10,5) as pct
+from msum, total
+union all
+  select    *
+  from total
+;
+ANALYSE :ne_wd_match_agg_sum:_pct;
+
+select * from :ne_wd_match_agg_sum:_pct;

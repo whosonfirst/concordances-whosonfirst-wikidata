ff648d033e31463c818a1e38ffeb741d0d21caf6
ImreSamu <ImreSamu@users.noreply.github.com>
2018-01-28 04:23:03 +0100

    add /ne_ok/*  export for OK/MAYBE record  ( safe bets )

--- code/cmd_export_ne_tables.sh
+++ code/cmd_export_ne_tables.sh
@@ -7,34 +7,31 @@ source_table=$1
 target_table=${1}
 target_sqlite3_table=$(echo "${target_table}" | cut -d'.' -f2)
 
+function neexport(){  
+nedir=$1 
+sqlfilter=$2
 
-mkdir -p ${outputdir}/ne
-
-
+mkdir -p ${nedir}
 ##  where substr(_matching_category,1,2) in ('OK','WA') 
 echo """
     -- export --
     \cd :reportdir 
-    \copy (SELECT * FROM ${source_table}  ORDER BY ogc_fid  ) TO '${target_table}.csv' DELIMITER ',' CSV HEADER ESCAPE '\"';
+    \copy (SELECT * FROM ${source_table}  ${sqlfilter}  ORDER BY ogc_fid  ) TO '${target_table}.csv' DELIMITER ',' CSV HEADER ESCAPE '\"';
+""" | psql -e -vreportdir="${nedir}"
 
-""" | psql -e -vreportdir="${outputdir}/ne"
-
-
-echo "${outputdir}/ne/${target_table}.db"
-
-sqlite3 -batch ${outputdir}/ne/${target_table}.db   <<EOF
-
+echo "${nedir}/${target_table}.db"
+sqlite3 -batch ${nedir}/${target_table}.db   <<EOF
 .mode csv
-.import ${outputdir}/ne/${target_table}.csv  ${target_sqlite3_table}
-
+.import ${nedir}/${target_table}.csv  ${target_sqlite3_table}
 EOF
 
-ls -la  ${outputdir}/ne/${target_table}.db
+ls -la  ${nedir}/${target_table}.db
+sqlite3  ${nedir}/${target_table}.db  "PRAGMA table_info( ${target_sqlite3_table})" > ${nedir}/${target_table}.dbstruct.txt
+ls -la  ${nedir}/${target_table}.*
+}
 
-sqlite3  ${outputdir}/ne/${target_table}.db  "PRAGMA table_info( ${target_sqlite3_table})" > ${outputdir}/ne/${target_table}.dbstruct.txt
 
+neexport ${outputdir}/ne     "where 1=1"
 
-#gzip ${outputdir}/ne/${target_table}.db
-#gzip ${outputdir}/ne/${target_table}.csv
+neexport ${outputdir}/ne_ok   "where substr(_matching_category,1,2) in ('OK','MA')"
 
-ls -la  ${outputdir}/ne/${target_table}.*
